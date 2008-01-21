947ff343d5bd097303b20a3d76fab9c14d8c842c
David Lutterkort <dlutter@redhat.com>
2008-01-21 14:06:19 -0800

    Some config settings - most of them need to be exposed through configure.ac

---
+++ src/config.h
@@ -0,0 +1,55 @@
+/*
+ * config.h: commonly used parameters. Most of them should eventually
+ * become buildtime cobnfigurables
+ *
+ * Copyright (C) 2007 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ * Author: David Lutterkort <dlutter@redhat.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+/* The env var that points to the chroot holding files we may modify.
+   Mostly useful for testing */
+#define AUGEAS_ROOT_ENV "AUGEAS_ROOT"
+
+/* Where the default spec files live. */
+#define AUGEAS_SPEC_DIR "/usr/share/augeas/spec"
+
+/* Name of env var that contains list of paths to search for additional
+   spec files */
+#define AUGEAS_SPEC_ENV "AUGEAS_SPECLIB"
+
+/* Fairly arbitrary bound on the length of the path we
+   accept from AUGEAS_SPEC_ENV */
+#define MAX_ENV_SIZE 4096
+
+/* Character separating paths in a list of paths */
+#define PATH_SEP_CHAR ':'
+
+#endif
+
+
+/*
+ * Local variables:
+ *  indent-tabs-mode: nil
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ *  tab-width: 4
+ * End:
+ */
