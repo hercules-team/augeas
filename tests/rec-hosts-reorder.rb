8e712b03d2b5b9d8873808f1759b4e83fa78dd73
David Lutterkort <dlutter@redhat.com>
2008-01-29 16:11:53 -0800

    Test that reordering of subtrees works

---
+++ tests/rec-hosts-reorder.rb
@@ -0,0 +1,26 @@
+# Delete the first hosts entry and then add it back into the tree
+# Because of how nodes are ordered, this should be equivalent to
+# moving the corresponding line to the end of the file
+commands="
+rm /system/config/hosts/0
+set /system/config/hosts/0/ipaddr 127.0.0.1
+set /system/config/hosts/0/canonical localhost.localdomain
+set /system/config/hosts/0/aliases/0 localhost
+set /system/config/hosts/0/aliases/1 galia.watzmann.net
+set /system/config/hosts/0/aliases/2 galia
+save
+"
+ 
+# Diff is bogus, but augtool is broken for this test, anyway
+diff["/etc/hosts"] = <<TXT
+--- /etc/hosts
++++ /etc/hosts.augnew
+@@ -1,6 +1,6 @@
+ # Do not remove the following line, or various programs
+ # that require network functionality will fail.
++172.31.122.14   orange.watzmann.net orange
+ 127.0.0.1\tlocalhost.localdomain\tlocalhost galia.watzmann.net galia
+ #172.31.122.254   granny.watzmann.net granny puppet
+ #172.31.122.1     galia.watzmann.net galia
+-172.31.122.14   orange.watzmann.net orange
+TXT
