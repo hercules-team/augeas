0b0882772ca6926517c37f0154bddc67875c85d6
David Lutterkort <dlutter@redhat.com>
2008-02-15 13:25:47 -0800

    Adjust expected diff
    Do not allow comments to disappear

--- tests/ini-yum-rmkey.rb
+++ tests/ini-yum-rmkey.rb
@@ -6,14 +6,12 @@ save
 diff["/etc/yum.conf"] = <<TXT
 --- /etc/yum.conf
 +++ /etc/yum.conf.augnew
-@@ -1,8 +1,7 @@
+@@ -1,7 +1,6 @@
  # Test for yum
  [main]
  cachedir=/var/cache/yum
 -keepcache=0
  debuglevel=2
--# some errant comment
+ # some errant comment
  installonly_limit=100
-+# some errant comment
- #and some blather at the end
 TXT
