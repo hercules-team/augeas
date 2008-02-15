917b5d1d25b703238573d620fe320466969b117a
David Lutterkort <dlutter@redhat.com>
2008-02-15 13:04:16 -0800

    Turn into a test on a legal tree and make it pass

--- tests/rec-rm-inner-field.rb
+++ tests/rec-rm-inner-field.rb
@@ -1,7 +1,5 @@
-# FIXME: Ultimately, this test should fail since it leads
-# to a file that is not syntactically correct
 commands="
-rm /system/config/pam/newrole/1/control
+rm /system/config/pam/newrole/3/opts
 save
 "
 
@@ -9,11 +7,11 @@ diff = {}
 diff["/etc/pam.d/newrole"] = <<TXT
 --- /etc/pam.d/newrole
 +++ /etc/pam.d/newrole.augnew
-@@ -1,5 +1,5 @@
- #%PAM-1.0
+@@ -1,5 +1,4 @@
+-#%PAM-1.0
  auth       include\tsystem-auth
--account    include\tsystem-auth
-+account    system-auth
+ account    include\tsystem-auth
  password   include\tsystem-auth
- session    required\tpam_namespace.so unmnt_remnt no_unmount_on_close
+-session    required\tpam_namespace.so unmnt_remnt no_unmount_on_close
++session    required\tpam_namespace.so
 TXT
