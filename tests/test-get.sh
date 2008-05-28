43a11961fdace8c2090ffa6ec4b72b401e7e61ff
David Lutterkort <dlutter@redhat.com>
2008-05-28 10:17:03 -0700

    Check that augtool reads all of tests/root/ without error

---
+++ tests/test-get.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Check that reading the files in tests/root/ with augtool does not lead to
+# any errors
+
+TOPDIR=$(cd $(dirname $0)/.. && pwd)
+[[ -n "$top_builddir" ]] || top_builddir=$TOPDIR
+[[ -n "$top_srcdir" ]] || top_srcdir=$TOPDIR
+
+AUGTOOL=${top_builddir}/src/augtool
+export AUGEAS_LENS_LIB=${top_srcdir}/lenses
+export AUGEAS_ROOT=${top_srcdir}/tests/root
+
+$AUGTOOL print '/augeas/files' | grep -q /error && ret=1 || ret=0
+exit $ret
