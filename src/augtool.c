5c171588371cee4dfe5ed4d1a2938459eba79b0c
David Lutterkort <dlutter@redhat.com>
2008-07-22 12:36:54 -0700

    Fix segfault when running 'augtool print'

--- src/augtool.c
+++ src/augtool.c
@@ -46,7 +46,7 @@ const char *root = NULL;
 char *loadpath = NULL;
 
 static char *cleanpath(char *path) {
-    if (strlen(path) == 0)
+    if (path == NULL || strlen(path) == 0)
         return path;
     char *e = path + strlen(path) - 1;
     while (e != path && (*e == SEP || isspace(*e)))
