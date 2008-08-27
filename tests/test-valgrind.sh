07cefc1777c92c78afe0d2e3364f268e7f72efba
David Lutterkort <dlutter@redhat.com>
2008-08-27 15:49:28 -0700

    Script to run lens tests through Valgrind
    We don't run this as part of 'make check' since it's _very_ slow.

---
+++ tests/test-valgrind.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Run the tests in lenses/tests through valgrind and report which ones leak
+set -e
+
+TOPDIR=$(cd $(dirname $0)/.. && pwd)
+[[ -n "$top_builddir" ]] || top_builddir=$TOPDIR
+[[ -n "$top_srcdir" ]] || top_srcdir=$TOPDIR
+
+
+AUGPARSE="libtool --mode=execute valgrind -q --leak-check=full ${top_builddir}/src/augparse"
+LENS_DIR=${top_srcdir}/lenses
+TESTS=$LENS_DIR/tests/test_*.aug
+
+for t in $TESTS
+do
+  echo Run $(basename $t .aug)
+  set +e
+  ${AUGPARSE} -I $LENS_DIR $t
+done
