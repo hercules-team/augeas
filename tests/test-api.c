hercules-team/augeas

commit e80494a61f11cf67bb38a3a45e6db27d47159e71

Rename struct path to struct pathx
Also rename path_* functions to pathx_*; rename make_pathx to pathx_parse
and change its signature to allow cleaner error reporting

src/augeas.c
@@ -232,36 +232,36 @@ struct augeas *aug_init(const char *root, const char *loadpath,
 }
 
 int aug_get(const struct augeas *aug, const char *path, const char **value) {
-    struct path *p = make_path(aug->origin->children, path);
+    struct pathx *p;
     struct tree *match;
     int r;
 
-    if (p == NULL)
+    if (pathx_parse(aug->origin->children, path, &p) != 0)
         return -1;
 
     if (value != NULL)
         *value = NULL;
 
-    r = path_find_one(p, &match);
+    r = pathx_find_one(p, &match);
     if (r == 1 && value != NULL)
         *value = match->value;
-    free_path(p);
+    free_pathx(p);
 
     return r;
 }
 
 struct tree *tree_set(struct tree *root, const char *path, const char *value) {
     struct tree *tree;
-    struct path *p = make_path(root, path);
+    struct pathx *p;
     int r;
 
-    if (p == NULL)
+    if (pathx_parse(root, path, &p) != 0)
         goto error;
 
-    r = path_expand_tree(p, &tree);
+    r = pathx_expand_tree(p, &tree);
     if (r == -1)
         goto error;
-    free_path(p);
+    free_pathx(p);
 
     if (tree->value != NULL) {
         free(tree->value);
@@ -275,7 +275,7 @@ struct tree *tree_set(struct tree *root, const char *path, const char *value) {
     tree->dirty = 1;
     return tree;
  error:
-    free_path(p);
+    free_pathx(p);
     return NULL;
 }
 
@@ -287,17 +287,16 @@ int tree_insert(struct tree *origin, const char *path, const char *label,
                 int before) {
     assert(origin->parent == origin);
 
-    struct path *p = NULL;
+    struct pathx *p = NULL;
     struct tree *new = NULL, *match;
 
     if (strchr(label, SEP) != NULL)
         return -1;
 
-    p = make_path(origin->children, path);
-    if (p == NULL)
+    if (pathx_parse(origin->children, path, &p) != 0)
         goto error;
 
-    if (path_find_one(p, &match) != 1)
+    if (pathx_find_one(p, &match) != 1)
         goto error;
 
     new = make_tree(strdup(label), NULL, match->parent, NULL);
@@ -310,11 +309,11 @@ int tree_insert(struct tree *origin, const char *path, const char *label,
         new->next = match->next;
         match->next = new;
     }
-    free_path(p);
+    free_pathx(p);
     return 0;
  error:
     free_tree(new);
-    free_path(p);
+    free_pathx(p);
     return -1;
 }
 
@@ -380,22 +379,21 @@ int tree_rm(struct tree *origin, const char *path) {
     assert(origin->parent == origin);
     assert(origin->next == NULL);
 
-    struct path *p = NULL;
+    struct pathx *p = NULL;
     struct tree *root = origin->children;
     struct tree *tree, **del;
     int cnt = 0, ndel = 0, i;
 
-    p = make_path(root, path);
-    if (p == NULL)
+    if (pathx_parse(root, path, &p) != 0)
         return -1;
 
-    for (tree = path_first(p); tree != NULL; tree = path_next(p, tree)) {
+    for (tree = pathx_first(p); tree != NULL; tree = pathx_next(p, tree)) {
         if (! TREE_HIDDEN(tree))
             ndel += 1;
     }
 
     if (ndel == 0) {
-        free_path(p);
+        free_pathx(p);
         return 0;
     }
 
@@ -404,13 +402,13 @@ int tree_rm(struct tree *origin, const char *path) {
         return -1;
     }
 
-    for (i = 0, tree = path_first(p); tree != NULL; tree = path_next(p, tree)) {
+    for (i = 0, tree = pathx_first(p); tree != NULL; tree = pathx_next(p, tree)) {
         if (TREE_HIDDEN(tree))
             continue;
         del[i] = tree;
         i += 1;
     }
-    free_path(p);
+    free_pathx(p);
 
     for (i = 0; i < ndel; i++) {
         assert (del[i]->parent != NULL);
@@ -451,20 +449,19 @@ int aug_tree_replace(struct augeas *aug, const char *path, struct tree *sub) {
 
 int aug_mv(struct augeas *aug, const char *src, const char *dst) {
     struct tree *root = aug->origin->children;
-    struct path *s = make_path(root, src);
-    struct path *d = make_path(root, dst);
+    struct pathx *s, *d;
     struct tree *ts, *td, *t;
     int r, ret;
 
     ret = -1;
-    if (s == NULL || d == NULL)
+    if (pathx_parse(root, src, &s) != 0 || pathx_parse(root, dst, &d) != 0)
         goto done;
 
-    r = path_find_one(s, &ts);
+    r = pathx_find_one(s, &ts);
     if (r != 1)
         goto done;
 
-    r = path_expand_tree(d, &td);
+    r = pathx_expand_tree(d, &td);
     if (r == -1)
         goto done;
 
@@ -498,13 +495,13 @@ int aug_mv(struct augeas *aug, const char *src, const char *dst) {
 
     ret = 0;
  done:
-    free_path(s);
-    free_path(d);
+    free_pathx(s);
+    free_pathx(d);
     return ret;
 }
 
 int aug_match(const struct augeas *aug, const char *pathin, char ***matches) {
-    struct path *p = NULL;
+    struct pathx *p = NULL;
     struct tree *tree;
     int cnt = 0;
 
@@ -515,15 +512,14 @@ int aug_match(const struct augeas *aug, const char *pathin, char ***matches) {
         pathin = "/*";
     }
 
-    p = make_path(aug->origin->children, pathin);
-    if (p == NULL)
+    if (pathx_parse(aug->origin->children, pathin, &p) != 0)
         return -1;
 
-    for (tree = path_first(p); tree != NULL; tree = path_next(p, tree)) {
+    for (tree = pathx_first(p); tree != NULL; tree = pathx_next(p, tree)) {
         if (! TREE_HIDDEN(tree))
             cnt += 1;
     }
-    free_path(p);
+    free_pathx(p);
     p = NULL;
 
     if (matches == NULL)
@@ -533,9 +529,10 @@ int aug_match(const struct augeas *aug, const char *pathin, char ***matches) {
     if (*matches == NULL)
         goto error;
 
-    p = make_path(aug->origin->children, pathin);
+    pathx_parse(aug->origin->children, pathin, &p);
+
     int i = 0;
-    for (tree = path_first(p); tree != NULL; tree = path_next(p, tree)) {
+    for (tree = pathx_first(p); tree != NULL; tree = pathx_next(p, tree)) {
         if (TREE_HIDDEN(tree))
             continue;
         (*matches)[i] = format_path(tree);
@@ -544,7 +541,7 @@ int aug_match(const struct augeas *aug, const char *pathin, char ***matches) {
         }
         i += 1;
     }
-    free_path(p);
+    free_pathx(p);
     return cnt;
 
  error:
@@ -555,7 +552,7 @@ int aug_match(const struct augeas *aug, const char *pathin, char ***matches) {
             free(*matches);
         }
     }
-    free_path(p);
+    free_pathx(p);
     return -1;
 }
 
@@ -642,17 +639,18 @@ static int update_save_flags(struct augeas *aug) {
 int aug_save(struct augeas *aug) {
     int ret = 0;
     struct tree *files;
-    struct path *p = NULL;
+    struct pathx *p = NULL;
 
     if (update_save_flags(aug) < 0)
         return -1;
 
-    p = make_path(aug->origin->children, AUGEAS_FILES_TREE);
-    if (p == NULL || path_find_one(p, &files) != 1) {
-        free_path(p);
+    if (pathx_parse(aug->origin->children, AUGEAS_FILES_TREE, &p) != 0)
+        return -1;
+    if (pathx_find_one(p, &files) != 1) {
+        free_pathx(p);
         return -1;
     }
-    free_path(p);
+    free_pathx(p);
 
     list_for_each(t, aug->origin->children) {
         tree_propagate_dirty(t);
@@ -721,15 +719,15 @@ static int print_rec(FILE *out, struct tree *start, const char *ppath,
 int print_tree(const struct tree *start, FILE *out, const char *pathin,
                int pr_hidden) {
 
-    struct path *p = make_path(start, pathin);
+    struct pathx *p;
     char *path = NULL;
     struct tree *tree;
     int r;
 
-    if (p == NULL)
+    if (pathx_parse(start, pathin, &p) != 0)
         return -1;
 
-    for (tree = path_first(p); tree != NULL; tree = path_next(p, tree)) {
+    for (tree = pathx_first(p); tree != NULL; tree = pathx_next(p, tree)) {
         if (TREE_HIDDEN(tree) && ! pr_hidden)
             continue;
 
@@ -745,7 +743,7 @@ int print_tree(const struct tree *start, FILE *out, const char *pathin,
         free(path);
         path = NULL;
     }
-    free_path(p);
+    free_pathx(p);
     return 0;
  error:
     free(path);

src/internal.h
@@ -350,14 +350,14 @@ int close_memstream(struct memstream *ms);
  * Path expressions
  */
 
-struct path;
+struct pathx;
 
-struct path *make_path(const struct tree *root, const char *path);
-struct tree *path_first(struct path *path);
-struct tree *path_next(struct path *path, struct tree *cur);
-int path_find_one(struct path *path, struct tree **match);
-int path_expand_tree(struct path *path, struct tree **tree);
-void free_path(struct path *path);
+int pathx_parse(const struct tree *root, const char *path, struct pathx **px);
+struct tree *pathx_first(struct pathx *path);
+struct tree *pathx_next(struct pathx *path, struct tree *cur);
+int pathx_find_one(struct pathx *path, struct tree **match);
+int pathx_expand_tree(struct pathx *path, struct tree **tree);
+void free_pathx(struct pathx *path);
 
 #endif
 

src/pathx.c
@@ -38,14 +38,14 @@ struct segment {
     char        *label;
 };
 
-struct path {
+struct pathx {
     size_t      nsegments;
     struct segment *segments;
     struct tree    *root;
 };
 
 #define for_each_segment(seg, path)                                 \
-    for (typeof(path->segments) (seg) = (path)->segments;           \
+    for (typeof((path)->segments) (seg) = (path)->segments;         \
          (seg) < (path)->segments + (path)->nsegments;              \
          (seg)++)
 
@@ -80,7 +80,7 @@ static int sibling_index(struct tree *tree) {
     return indx;
 }
 
-void free_path(struct path *path) {
+void free_pathx(struct pathx *path) {
     if (path == NULL)
         return;
 
@@ -102,32 +102,31 @@ void free_path(struct path *path) {
  * SEGMENT = STRING ('[' N ']') ? | '*'
  * where STRING is any string not containing '/' and N is a positive number
  */
-struct path *make_path(const struct tree *root, const char *path) {
-    struct path *result;
+int pathx_parse(const struct tree *root, const char *path,
+                struct pathx **px) {
 
-    CALLOC(result, 1);
-    if (result == NULL)
-        return NULL;
-    result->root = (struct tree *) root;
+    if (ALLOC(*px) < 0)
+        return -1;
+    (*px)->root = (struct tree *) root;
 
     if (*path != SEP)
-        result->nsegments = 1;
+        (*px)->nsegments = 1;
     for (const char *p = path; *p != '\0'; p++) {
         if (*p == SEP) {
             while (*p == SEP) p++;
             if (*p == '\0')
                 break;
-            result->nsegments++;
+            (*px)->nsegments++;
         }
     }
-    if (result->nsegments == 0)
+    if ((*px)->nsegments == 0)
         goto error;
 
-    CALLOC(result->segments, result->nsegments);
-    if (result->segments == NULL)
+    CALLOC((*px)->segments, (*px)->nsegments);
+    if ((*px)->segments == NULL)
         goto error;
 
-    for_each_segment(seg, result) {
+    for_each_segment(seg, *px) {
         const char *next, *brack;
 
         while (*path == SEP)
@@ -162,11 +161,11 @@ struct path *make_path(const struct tree *root, const char *path) {
         path = next;
         while (*path == SEP) path += 1;
     }
-    return result;
+    return 0;
 
  error:
-    free_path(result);
-    return NULL;
+    free_pathx(*px);
+    return -1;
 }
 
 static void calc_last_index(struct segment *seg, struct tree *tree) {
@@ -205,7 +204,7 @@ static int tree_next(struct tree **tree, int *depth, int mindepth) {
 
 /* Given a node TREE that should match the segment SEGNR in PATH,
    find the next node that matches all of PATH, or return NULL */
-static struct tree *complete_path(struct path *path, int segnr,
+static struct tree *complete_path(struct pathx *path, int segnr,
                                   struct tree *tree) {
     int cur = segnr;
 
@@ -230,7 +229,7 @@ static struct tree *complete_path(struct path *path, int segnr,
     }
 }
 
-struct tree *path_next(struct path *path, struct tree *cur) {
+struct tree *pathx_next(struct pathx *path, struct tree *cur) {
     int segnr = path->nsegments - 1;
 
     if (tree_next(&cur, &segnr, -1) < 0)
@@ -240,7 +239,7 @@ struct tree *path_next(struct path *path, struct tree *cur) {
 }
 
 /* Find the first node in TREE matching PATH. */
-struct tree *path_first(struct path *path) {
+struct tree *pathx_first(struct pathx *path) {
     return complete_path(path, 0, path->root);
 }
 
@@ -253,7 +252,7 @@ struct tree *path_first(struct path *path) {
  * number of the segment in PATH where MATCH matched. If no node matches,
  * MATCH will be NULL, and SEGNR -1
  */
-static int path_search(struct path *path, struct tree **match, int *segnr) {
+static int path_search(struct pathx *path, struct tree **match, int *segnr) {
     struct tree **matches = NULL;
     int *nmatches = NULL;
     int result;
@@ -323,7 +322,7 @@ static int path_search(struct path *path, struct tree **match, int *segnr) {
  * Return the first segment that was created by this operation, or NULL on
  * error.
  */
-int path_expand_tree(struct path *path, struct tree **tree) {
+int pathx_expand_tree(struct pathx *path, struct tree **tree) {
     int r, segnr;
 
     *tree = path->root;
@@ -364,12 +363,12 @@ int path_expand_tree(struct path *path, struct tree **tree) {
     return -1;
 }
 
-int path_find_one(struct path *path, struct tree **tree) {
-    *tree = path_first(path);
+int pathx_find_one(struct pathx *path, struct tree **tree) {
+    *tree = pathx_first(path);
     if (*tree == NULL)
         return 0;
 
-    if (path_next(path, *tree) != NULL) {
+    if (pathx_next(path, *tree) != NULL) {
         *tree = NULL;
         return -1;
     }

commit c99e551e6468d30d409c6a8cb48ac270de0771fb

aug_get: return -1 when multiple nodes match
This fixes breakage in the API contract for aug_get, introduced in commit
e80494a6. Also adds a test to ensure we do not break API for aug_get again.

Fixes bug #121

.gitignore
@@ -35,6 +35,7 @@ src/augtool
 src/augparse
 tests/fatest
 tests/lens-*.sh
+tests/test-api
 tests/test-xpath
 tests/test-load
 tests/test-save

src/augeas.c
@@ -484,7 +484,7 @@ static int find_one_node(struct pathx *p, struct tree **match) {
 }
 
 int aug_get(const struct augeas *aug, const char *path, const char **value) {
-    struct pathx *p;
+    struct pathx *p = NULL;
     struct tree *match;
     int r;
 
@@ -497,6 +497,10 @@ int aug_get(const struct augeas *aug, const char *path, const char **value) {
         *value = NULL;
 
     r = pathx_find_one(p, &match);
+    ERR_BAIL(aug);
+    ERR_THROW(r > 1, aug, AUG_EMMATCH, "There are %d nodes matching %s",
+              r, path);
+
     if (r == 1 && value != NULL)
         *value = match->value;
     free_pathx(p);
@@ -504,6 +508,7 @@ int aug_get(const struct augeas *aug, const char *path, const char **value) {
     api_exit(aug);
     return r;
  error:
+    free_pathx(p);
     api_exit(aug);
     return -1;
 }

tests/Makefile.am
@@ -118,7 +118,7 @@ EXTRA_DIST = \
 
 noinst_SCRIPTS = $(check_SCRIPTS)
 
-check_PROGRAMS = fatest test-xpath test-load test-save
+check_PROGRAMS = fatest test-xpath test-load test-save test-api
 
 TESTS_ENVIRONMENT = \
   PATH='$(abs_top_builddir)/src$(PATH_SEPARATOR)'"$$PATH" \
@@ -141,6 +141,9 @@ test_load_LDADD = $(top_builddir)/src/libaugeas.la $(GNULIB)
 
 test_save_SOURCES = test-save.c cutest.c cutest.h $(top_srcdir)/src/memory.c $(top_srcdir)/src/memory.h
 test_save_LDADD = $(top_builddir)/src/libaugeas.la $(GNULIB)
+
+test_api_SOURCES = test-api.c cutest.c cutest.h $(top_srcdir)/src/memory.c $(top_srcdir)/src/memory.h
+test_api_LDADD = $(top_builddir)/src/libaugeas.la $(GNULIB)
 
 FAILMALLOC_START ?= 1
 FAILMALLOC_REP   ?= 20

tests/test-api.c
@@ -1,0 +1,109 @@
+/*
+ * test-api.c: test public API functions for conformance
+ *
+ * Copyright (C) 2009 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ * Author: David Lutterkort <lutter@redhat.com>
+ */
+
+#include <config.h>
+
+#include "augeas.h"
+
+#include "cutest.h"
+#include "internal.h"
+
+static const char *abs_top_srcdir;
+static char *root;
+static char *loadpath;
+
+#define die(msg)                                                    \
+    do {                                                            \
+        fprintf(stderr, "%d: Fatal error: %s\n", __LINE__, msg);    \
+        exit(EXIT_FAILURE);                                         \
+    } while(0)
+
+static void testGet(CuTest *tc) {
+    int r;
+    const char *value;
+    struct augeas *aug;
+
+    aug = aug_init(root, loadpath, AUG_NO_STDINC|AUG_NO_LOAD);
+    CuAssertPtrNotNull(tc, aug);
+    CuAssertIntEquals(tc, AUG_NOERROR, aug_error(aug));
+
+    /* Make sure we're looking at the right thing */
+    r = aug_match(aug, "/augeas/version/save/*", NULL);
+    CuAssertTrue(tc, r > 1);
+    CuAssertIntEquals(tc, AUG_NOERROR, aug_error(aug));
+
+    /* aug_get returns 1 and the value if exactly one node matches */
+    r = aug_get(aug, "/augeas/version/save/*[1]", &value);
+    CuAssertIntEquals(tc, 1, r);
+    CuAssertPtrNotNull(tc, value);
+    CuAssertIntEquals(tc, AUG_NOERROR, aug_error(aug));
+
+    /* aug_get returns 0 and no value when no node matches */
+    r = aug_get(aug, "/augeas/version/save/*[ last() + 1 ]", &value);
+    CuAssertIntEquals(tc, 0, r);
+    CuAssertPtrEquals(tc, NULL, value);
+    CuAssertIntEquals(tc, AUG_NOERROR, aug_error(aug));
+
+    /* aug_get should return an error when multiple nodes match */
+    r = aug_get(aug, "/augeas/version/save/*", &value);
+    CuAssertIntEquals(tc, -1, r);
+    CuAssertPtrEquals(tc, NULL, value);
+    CuAssertIntEquals(tc, AUG_EMMATCH, aug_error(aug));
+
+    aug_close(aug);
+}
+
+int main(void) {
+    char *output = NULL;
+    CuSuite* suite = CuSuiteNew();
+    CuSuiteSetup(suite, NULL, NULL);
+
+    SUITE_ADD_TEST(suite, testGet);
+
+    abs_top_srcdir = getenv("abs_top_srcdir");
+    if (abs_top_srcdir == NULL)
+        die("env var abs_top_srcdir must be set");
+
+    if (asprintf(&root, "%s/tests/root", abs_top_srcdir) < 0) {
+        die("failed to set root");
+    }
+
+    if (asprintf(&loadpath, "%s/lenses", abs_top_srcdir) < 0) {
+        die("failed to set loadpath");
+    }
+
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, &output);
+    CuSuiteDetails(suite, &output);
+    printf("%s\n", output);
+    free(output);
+    return suite->failCount;
+}
+
+/*
+ * Local variables:
+ *  indent-tabs-mode: nil
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ *  tab-width: 4
+ * End:
+ */