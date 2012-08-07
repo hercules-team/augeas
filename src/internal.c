225e2c0428578469461d3d6fe82f3ff750936c25
Francis Giraldeau <francis.giraldeau@gmail.com>
2012-08-07 13:05:52 +0200

    Escape double quotes when dumping regexp
    Graphviz DOT is using double quotes to quotes the string of a label. Escape
    double quotes to avoid parsing errors with graphviz when the regexp of the lens
    includes double quotes.

--- src/internal.c
+++ src/internal.c
@@ -248,7 +248,7 @@ int print_chars(FILE *out, const char *text, int cnt) {
     if (cnt < 0)
         cnt = strlen(text);
 
-    esc = escape(text, cnt, NULL);
+    esc = escape(text, cnt, "\"");
     total = strlen(esc);
     if (out != NULL)
         fprintf(out, "%s", esc);
