hercules-team/augeas

commit 4e09d159a19204a6f7590a79dd60ed3cbb30cf71

Grub: support the 'device' directive for UEFI boot
Fixes bug #189

lenses/grub.aug
@@ -40,6 +40,12 @@ module Grub =
 
     let kw_pres (kw:string) = [ opt_ws . key kw . del_to_eol . eol ]
 
+    (* This is a shell-only directive in upstream grub; the grub versions
+       in at least Fedora/RHEL use this to find devices for UEFI boot *)
+    let device =
+	  [ command "device" "" . Sep.space . store /\([A-Za-z0-9_.-]+\)/ . spc .
+		  [ label "file" . value_to_eol ] . Util.eol ]
+
     let color =
       (* Should we nail it down to exactly the color names that *)
       (* grub supports ? *)
@@ -75,6 +81,7 @@ module Grub =
                      | terminal
                      | password_arg
                      | color
+		     | device
 
     let title = del /title[ \t=]+/ "title " . value_to_eol . eol
 

lenses/tests/test_grub.aug
@@ -9,6 +9,7 @@ module Test_grub =
 #          kernel /vmlinuz-version ro root=/dev/vg00/lv00
 #          initrd /initrd-version.img
 #boot=/dev/sda
+device (hd0) HD(1,800,64000,9895c137-d4b2-4e3b-a93b-dc9ac4)
 password --md5 $1$M9NLj$p2gs87vwNv48BUu.wAfVw0
 default=0
 background 103332
@@ -48,6 +49,8 @@ title othermenu
     { "#comment" = "kernel /vmlinuz-version ro root=/dev/vg00/lv00" }
     { "#comment" = "initrd /initrd-version.img" }
     { "#comment" = "boot=/dev/sda" }
+    { "device"   = "(hd0)"
+	    { "file" = "HD(1,800,64000,9895c137-d4b2-4e3b-a93b-dc9ac4)" } }
     { "password" = "$1$M9NLj$p2gs87vwNv48BUu.wAfVw0"
         { "md5" } }
     { "default" = "0" }

commit f55175ed0e17a5a4f50a32d9ad5cba5f486eb577

Grub: NFC fix whitespace errors introduced by 4e09d15
Kill tabs!

lenses/grub.aug
@@ -102,8 +102,8 @@ module Grub =
      *  This is a shell-only directive in upstream grub; the grub versions
      *  in at least Fedora/RHEL use this to find devices for UEFI boot *)
     let device =
-	  [ command "device" "" . Sep.space . store /\([A-Za-z0-9_.-]+\)/ . spc .
-		  [ label "file" . value_to_eol ] . Util.eol ]
+      [ command "device" "" . Sep.space . store /\([A-Za-z0-9_.-]+\)/ . spc .
+        [ label "file" . value_to_eol ] . Util.eol ]
 
     (* View: color *)
     let color =
@@ -150,7 +150,7 @@ module Grub =
                      | terminal
                      | password_arg
                      | color
-		     | device
+                     | device
                      | setkey
 
     (* View: title *)

lenses/tests/test_grub.aug
@@ -52,7 +52,7 @@ title othermenu
     { "#comment" = "initrd /initrd-version.img" }
     { "#comment" = "boot=/dev/sda" }
     { "device"   = "(hd0)"
-	    { "file" = "HD(1,800,64000,9895c137-d4b2-4e3b-a93b-dc9ac4)" } }
+        { "file" = "HD(1,800,64000,9895c137-d4b2-4e3b-a93b-dc9ac4)" } }
     { "password" = "$1$M9NLj$p2gs87vwNv48BUu.wAfVw0"
         { "md5" } }
     { "default" = "0" }