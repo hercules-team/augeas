acfa635f99d03791ebfbeeacd98ba963bae842f2
Robert Drake <rdrake@direcpath.com>
2014-03-08 04:26:29 -0500

    * src/augtool.c: fix for readline add_history with interactive mode
    If you use interactive mode, augtool never adds anything to history that
    you type once the input becomes a normal tty.
    
    This is because it was checking stdin, which as far as the system is
    concerned is still !isatty().

--- src/augtool.c
+++ src/augtool.c
@@ -490,10 +490,11 @@ static int main_loop(void) {
                 echo_commands = true;
 
                 // reopen in stream
-                if ((rl_instream = fopen("/dev/tty", "r")) == NULL) {
+                if (freopen("/dev/tty", "r", stdin) == NULL) {
                     perror("Failed to open terminal for reading");
                     return -1;
                 }
+                rl_instream = stdin;
 
                 // reopen stdout and stream to a tty if originally silenced or
                 // not connected to a tty, for full interactive mode
