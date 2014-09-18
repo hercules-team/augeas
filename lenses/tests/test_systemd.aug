hercules-team/augeas

commit 5226ae7d162691c20e00096ae8023e74b912ace0

Systemd: allow quoted Environment key=value pairs
Fixes RHBZ#1100547

lenses/systemd.aug
@@ -125,8 +125,13 @@ let entry_command =
 (* View: entry_env
    Entry that takes a space separated set of ENV=value key/value pairs *)
 let entry_env =
-     let envkv = [ key env_key . Util.del_str "=" . ( sto_value )? ]
-  in entry_fn entry_env_kw ( Build.opt_list envkv value_sep )
+     let envkv (env_val:lens) = key env_key . Util.del_str "=" . env_val
+     (* bare has no spaces, and is optionally quoted *)
+  in let bare = Quote.do_quote_opt (envkv (store /[^;#'" \t\n]*[^;#'" \t\n\\]/)?)
+     (* quoted has at least one space, and must be quoted *)
+  in let quoted = Quote.do_quote (envkv (store /[^;#"'\n]*[ \t]+[^;#"'\n]*/))
+  in let envkv_quoted = [ bare ] | [ quoted ]
+  in entry_fn entry_env_kw ( Build.opt_list envkv_quoted value_sep )
 
 
 (************************************************************************

lenses/tests/test_systemd.aug
@@ -203,6 +203,7 @@ Environment=LANG=C FOO=BAR
 Environment=LANG= LANGUAGE= LC_CTYPE= LC_NUMERIC= LC_TIME= LC_COLLATE= LC_MONETARY= LC_MESSAGES= LC_PAPER= LC_NAME= LC_ADDRESS= LC_TELEPHONE= LC_MEASUREMENT= LC_IDENTIFICATION=
 Environment=LANG=C\
 FOO=BAR
+Environment=\"LANG=foo bar\" FOO=BAR
 "
 (* Test: Systemd.lns *)
 test Systemd.lns get env =
@@ -232,6 +233,10 @@ test Systemd.lns get env =
     }
     { "Environment"
       { "LANG" = "C" }
+      { "FOO" = "BAR" }
+    }
+    { "Environment"
+      { "LANG" = "foo bar" }
       { "FOO" = "BAR" }
     }
   }
@@ -302,4 +307,3 @@ test Systemd.entry_command get "ExecStart=/usr/bin/find /var/lib/sudo -exec /usr
       { "7" = "\073" }
     }
   }
-

commit f64d8bc7a7670f3af2549fdcefb64c2b5f22cd0d

Systemd: parse environment variables where only value is quoted
Fixes regression introduced in 5226ae7, which added support to parse fully
quoted "NAME=value" environment vars.  To support both, this commit stores
the quotes in the value if it's only the value quoted.

Fixes RHBZ#1138508

lenses/systemd.aug
@@ -132,9 +132,11 @@ let entry_env =
      let envkv (env_val:lens) = key env_key . Util.del_str "=" . env_val
      (* bare has no spaces, and is optionally quoted *)
   in let bare = Quote.do_quote_opt (envkv (store /[^#'" \t\n]*[^#'" \t\n\\]/)?)
+  in let bare_dqval = envkv (store /"[^#" \t\n]*[^#" \t\n\\]"/)
+  in let bare_sqval = envkv (store /'[^#' \t\n]*[^#' \t\n\\]'/)
      (* quoted has at least one space, and must be quoted *)
   in let quoted = Quote.do_quote (envkv (store /[^#"'\n]*[ \t]+[^#"'\n]*/))
-  in let envkv_quoted = [ bare ] | [ quoted ]
+  in let envkv_quoted = [ bare ] | [ bare_dqval ] | [ bare_sqval ] | [ quoted ]
   in entry_fn entry_env_kw ( Build.opt_list envkv_quoted value_sep )
 
 

lenses/tests/test_systemd.aug
@@ -204,6 +204,8 @@ Environment=LANG= LANGUAGE= LC_CTYPE= LC_NUMERIC= LC_TIME= LC_COLLATE= LC_MONETA
 Environment=LANG=C\
 FOO=BAR
 Environment=\"LANG=foo bar\" FOO=BAR
+Environment=OPTIONS=\"-LS0-6d\"
+Environment=OPTIONS='-LS0-6d'
 "
 (* Test: Systemd.lns *)
 test Systemd.lns get env =
@@ -238,6 +240,12 @@ test Systemd.lns get env =
     { "Environment"
       { "LANG" = "foo bar" }
       { "FOO" = "BAR" }
+    }
+    { "Environment"
+      { "OPTIONS" = "\"-LS0-6d\"" }
+    }
+    { "Environment"
+      { "OPTIONS" = "'-LS0-6d'" }
     }
   }
 