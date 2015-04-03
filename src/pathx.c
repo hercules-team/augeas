hercules-team/augeas

commit e4f0a6773481423b971667f5d3c0b4f4e830b3e7

* src/pathx.c (pathx_escape_name): new function
Several characters in a path component are treated special, such as '[' and
']'; to treat a string containing such characters as a literal path
component, they need to be escaped with a '\\'.

src/pathx.c
@@ -1616,6 +1616,33 @@ static void push_new_binary_op(enum binary_op op, struct state *state) {
     push_expr(expr, state);
 }
 
+int pathx_escape_name(const char *in, char **out) {
+    const char *p;
+    int num_to_escape = 0;
+    char *s;
+
+    *out = NULL;
+
+    for (p = in; *p; p++) {
+        if (strchr(name_follow, *p) || isspace(*p))
+            num_to_escape += 1;
+    }
+
+    if (num_to_escape == 0)
+        return 0;
+
+    if (ALLOC_N(*out, strlen(in) + num_to_escape + 1) < 0)
+        return -1;
+
+    for (p = in, s = *out; *p; p++) {
+        if (strchr(name_follow, *p) || isspace(*p))
+            *s++ = '\\';
+        *s++ = *p;
+    }
+    *s = '\0';
+    return 0;
+}
+
 /*
  * NameNoWS ::= [^][|/\= \t\n] | \\.
  * NameWS   ::= [^][|/\=] | \\.

src/internal.h
@@ -575,6 +575,16 @@ void pathx_symtab_remove_descendants(struct pathx_symtab *symtab,
                                      const struct tree *tree);
 void free_symtab(struct pathx_symtab *symtab);
 
+/* Escape a name so that it is safe to pass to parse_name and have it
+ * interpreted as the literal name of a path component.
+ *
+ * On return, *OUT will be NULL if IN does not need escaping, otherwise it
+ * will contain an escaped copy of IN which the caller must free.
+ *
+ * Returns -1 if it failed to allocate memory for *OUT, 0 on success
+ */
+int pathx_escape_name(const char *in, char **out);
+
 /* Debug helpers, all defined in internal.c. When ENABLE_DEBUG is not
  * set, they compile to nothing.
  */

commit f7c2174e5b97de1a68d77599f5711db53b6d158d

* src/pathx.c (pathx_escape_name): also escape backslash
Since backslash is an escape character in parse_name, we need to escape it
with a backslash to make sure that parse_name is really the inverse of
pathx_escape_name.

Thanks to Eric Blake for spotting this problem with the initial attempt to
escape names introduced in e4f0a677

src/pathx.c
@@ -1624,7 +1624,7 @@ int pathx_escape_name(const char *in, char **out) {
     *out = NULL;
 
     for (p = in; *p; p++) {
-        if (strchr(name_follow, *p) || isspace(*p))
+        if (strchr(name_follow, *p) || isspace(*p) || *p == '\\')
             num_to_escape += 1;
     }
 
@@ -1635,7 +1635,7 @@ int pathx_escape_name(const char *in, char **out) {
         return -1;
 
     for (p = in, s = *out; *p; p++) {
-        if (strchr(name_follow, *p) || isspace(*p))
+        if (strchr(name_follow, *p) || isspace(*p) || *p == '\\')
             *s++ = '\\';
         *s++ = *p;
     }