731466ec1b47237df5f238f6dfe46b019f66c8b0
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-09-24 16:47:04 +0200

    better var name

--- src/auglua.c
+++ src/auglua.c
@@ -29,10 +29,10 @@
 #include <stdbool.h>
 
 
-static const char *Key = "augeas_registry_key";
+static const char *registry_key = "augeas_registry_key";
 
 static augeas *checkaug(lua_State *L) {
-  lua_pushlightuserdata(L, (void *)&Key);
+  lua_pushlightuserdata(L, (void *)&registry_key);
   lua_gettable(L, LUA_REGISTRYINDEX);
   augeas *aug = (augeas *)lua_touserdata(L, -1); // Convert value
   return aug;
@@ -469,7 +469,7 @@ struct lua_State *luaopen_augeas(augeas *a) {
   
     // lightuserdata is shared between libs
     // do we really want to use that?
-    lua_pushlightuserdata(L, (void *)&Key);
+    lua_pushlightuserdata(L, (void *)&registry_key);
     lua_pushlightuserdata(L, (void *)a); // Push pointer
     lua_settable(L, LUA_REGISTRYINDEX);
 
