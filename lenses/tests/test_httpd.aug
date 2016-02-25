hercules-team/augeas

commit 0b22176535809f6e8aba3191a809f122e08bc7d0

Httpd: parse line continuations in section arguments

lenses/httpd.aug
@@ -45,9 +45,8 @@ autoload xfm
 let dels (s:string)     = del s s
 
 (* deal with continuation lines *)
-let sep_spc    =  del /([ \t]+|[ \t]*\\\\\r?\n[ \t]*)/ " "
-
-let sep_osp             = Sep.opt_space
+let sep_spc             = del /([ \t]+|[ \t]*\\\\\r?\n[ \t]*)/ " "
+let sep_osp             = del /([ \t]*|[ \t]*\\\\\r?\n[ \t]*)/ ""
 let sep_eq              = del /[ \t]*=[ \t]*/ "="
 
 let nmtoken             = /[a-zA-Z:_][a-zA-Z0-9:_.-]*/
@@ -60,7 +59,7 @@ let indent              = Util.indent
 
 (* borrowed from shellvars.aug *)
 let char_arg_dir  = /([^\\ '"{\t\r\n]|[^ '"{\t\r\n]+[^\\ \t\r\n])|\\\\"|\\\\'/
-let char_arg_sec  = /[^ '"\t\r\n>]|\\\\"|\\\\'/
+let char_arg_sec  = /[^\\ '"\t\r\n>]|\\\\"|\\\\'/
 let char_arg_wl   = /([^\\ '"},\t\r\n]|[^ '"},\t\r\n]+[^\\ '"},\t\r\n])/
 
 let cdot = /\\\\./

lenses/tests/test_httpd.aug
@@ -538,3 +538,16 @@ test Httpd.lns get "<Perl>
 </Perl>\n" =
   { "Perl" = "\n    Apache::AuthDBI->setCacheTime(600);\n" }
 
+(* Line continuations inside VirtualHost blocks *)
+test Httpd.lns get "<VirtualHost \\
+    0.0.0.0:7080 \\
+    [00000:000:000:0000::2]:7080 \\
+    0.0.0.0:7080 \\
+    127.0.0.1:7080 \\
+    >
+</VirtualHost>\n" =
+  { "VirtualHost"
+    { "arg" = "0.0.0.0:7080" }
+    { "arg" = "[00000:000:000:0000::2]:7080" }
+    { "arg" = "0.0.0.0:7080" }
+    { "arg" = "127.0.0.1:7080" } }

commit 1cd33e52110e7c85befc00d93c867ec89cc12628

Httpd: permit backslashes in section tags
Fixes a regression introduced by 0b22176 which failed when a backslash
was present in a section tag.  Now it's permitted apart from being the
last character, when it's taken to be a line continuation.

lenses/httpd.aug
@@ -59,7 +59,7 @@ let indent              = Util.indent
 
 (* borrowed from shellvars.aug *)
 let char_arg_dir  = /([^\\ '"{\t\r\n]|[^ '"{\t\r\n]+[^\\ \t\r\n])|\\\\"|\\\\'/
-let char_arg_sec  = /[^\\ '"\t\r\n>]|\\\\"|\\\\'/
+let char_arg_sec  = /([^\\ '"\t\r\n>]|[^ '"\t\r\n>]+[^\\ \t\r\n>])|\\\\"|\\\\'/
 let char_arg_wl   = /([^\\ '"},\t\r\n]|[^ '"},\t\r\n]+[^\\ '"},\t\r\n])/
 
 let cdot = /\\\\./

lenses/tests/test_httpd.aug
@@ -551,3 +551,12 @@ test Httpd.lns get "<VirtualHost \\
     { "arg" = "[00000:000:000:0000::2]:7080" }
     { "arg" = "0.0.0.0:7080" }
     { "arg" = "127.0.0.1:7080" } }
+
+(* Non-continuation backslashes inside VirtualHost section headings *)
+test Httpd.lns get "<FilesMatch \.php$>
+  ExpiresActive Off
+</FilesMatch>\n" =
+  { "FilesMatch"
+    { "arg" = "\.php$" }
+    { "directive" = "ExpiresActive"
+      { "arg" = "Off" } } }