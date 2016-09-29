08fc497a3876ca476d68da3123cd4779cf5e50c0
Omer Katz <omer.drow@gmail.com>
2016-09-29 21:47:26 +0300

    Show test suite error log.

--- ci/test.sh
+++ ci/test.sh
@@ -13,10 +13,15 @@ end_fold_marker() {
 start_fold_marker 'configure'
 ./configure --enable-debug=yes
 end_fold_marker 'configure'
+
 start_fold_marker 'compilation'
 make -j 2
-make check -j 2
 end_fold_marker 'compilation'
+
 start_fold_marker 'test'
-./src/try valgrind
+make check -j 2 || exit_status=$1 && echo gnulib/tests/test-suite.log && exit $exit_status
 end_fold_marker 'test'
+
+start_fold_marker 'test.lenses'
+./src/try valgrind
+end_fold_marker 'test.lenses'
