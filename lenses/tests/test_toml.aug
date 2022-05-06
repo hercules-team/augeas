hercules-team/augeas

commit 5e1cd313aae78b727787c43bd48607bfc84da1ab

TOML lens (#91)
Toml: New lens to parse the TOML format

lenses/toml.aug
@@ -1,0 +1,145 @@
+(*
+Module: Toml
+  Parses TOML files
+
+Author: Raphael Pinson <raphael.pinson@camptocamp.com>
+
+About: Reference
+  https://github.com/mojombo/toml/blob/master/README.md
+
+About: License
+   This file is licenced under the LGPL v2+, like the rest of Augeas.
+
+About: Lens Usage
+   To be documented
+
+About: Configuration files
+   This lens applies to TOML files.
+
+About: Examples
+   The <Test_Toml> file contains various examples and tests.
+*)
+
+module Toml =
+
+(* Group: base definitions *)
+
+(* View: comment
+     A simple comment *)
+let comment  = IniFile.comment "#" "#"
+
+(* View: empty
+     An empty line *)
+let empty = Util.empty
+
+(* View: eol
+     An end of line *)
+let eol = Util.eol
+
+
+(* Group: value entries *)
+
+let bare_re_noquot = (/[^][", \t\r\n]/ - "#")
+let bare_re = (/[^][,\r=]/ - "#")+
+let no_quot = /[^]["\r\n]*/
+let bare = Quote.do_dquote_opt_nil (store (bare_re_noquot . (bare_re* . bare_re_noquot)?))
+let quoted = Quote.do_dquote (store (/[^"]/ . "#"* . /[^"]/))
+
+let ws = del /[ \t\n]*/ ""
+
+let space_or_empty = [ del /[ \t\n]+/ " " ]
+
+let comma = Util.del_str "," . (space_or_empty | comment)?
+let lbrace = Util.del_str "{" . (space_or_empty | comment)?
+let rbrace = Util.del_str "}"
+let lbrack = Util.del_str "[" . (space_or_empty | comment)?
+let rbrack = Util.del_str "]"
+
+(* This follows the definition of 'string' at https://www.json.org/
+   It's a little wider than what's allowed there as it would accept
+   nonsensical \u escapes *)
+let triple_dquote = Util.del_str "\"\"\""
+let str_store = Quote.dquote . store /([^\\"]|\\\\["\/bfnrtu\\])*/ . Quote.dquote
+
+let str_store_multi = triple_dquote . eol
+                  . store /([^\\"]|\\\\["\/bfnrtu\\])*/
+                  . del /\n[ \t]*/ "\n" . triple_dquote
+
+let str_store_literal = Quote.squote . store /([^\\']|\\\\['\/bfnrtu\\])*/ . Quote.squote
+
+let integer =
+     let base10 = /[+-]?[0-9_]+/
+  in let hex = /0x[A-Za-z0-9]+/
+  in let oct = /0o[0-7]+/
+  in let bin = /0b[01]+/
+  in [ label "integer" . store (base10 | hex | oct | bin) ]
+
+let float =
+     let n = /[0-9_]+/
+  in let pm = /[+-]?/
+  in let z = pm . n
+  in let decim = "." . n
+  in let exp = /[Ee]/ . z
+  in let num = z . decim | z . exp | z . decim . exp
+  in let inf = pm . "inf"
+  in let nan = pm . "nan"
+  in [ label "float" . store (num | inf | nan) ]
+
+let str = [ label "string" . str_store ]
+
+let str_multi = [ label "string_multi" . str_store_multi ]
+
+let str_literal = [ label "string_literal" . str_store_literal ]
+
+let bool (r:regexp) = [ label "bool" . store r ]
+
+
+let date_re = /[0-9]{4}-[0-9]{2}-[0-9]{2}/
+let time_re = /[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?[A-Z]*/
+
+let datetime = [ label "datetime" . store (date_re . /[T ]/ . time_re) ]
+let date = [ label "date" . store date_re ]
+let time = [ label "time" . store time_re ]
+
+let norec = str | str_multi | str_literal
+          | integer | float | bool /true|false/
+          | datetime | date |  time
+
+let array (value:lens) = [ label "array" . lbrack
+               . ( ( Build.opt_list value comma . space_or_empty? . rbrack )
+                   | rbrack ) ]
+
+let array_norec = array norec
+
+let rec array_rec = array (norec | array_rec)
+
+let entry_base (value:lens) = [ label "entry" . store Rx.word . Sep.space_equal . value ]
+
+let inline_table (value:lens) = [ label "inline_table" . lbrace
+                   . ( (Build.opt_list (entry_base value) comma . space_or_empty? . rbrace)
+                      | rbrace ) ]
+
+let entry = [ label "entry" . Util.indent . store Rx.word . Sep.space_equal
+            . (norec | array_rec | inline_table norec) . (eol | comment) ]
+
+(* Group: tables *)
+
+(* View: table_gen
+     A generic table *)
+let table_gen (name:string) (lbrack:string) (rbrack:string) =
+     let title = Util.indent . label name
+               . Util.del_str lbrack
+               . store /[^]\r\n.]+(\.[^]\r\n.]+)*/
+               . Util.del_str rbrack . eol
+  in [ title . (entry|empty|comment)* ]
+
+(* View: table
+     A table or array of tables *)
+let table = table_gen "table" "[" "]"
+          | table_gen "@table" "[[" "]]"
+
+(* Group: lens *)
+
+(* View: lns
+     The Toml lens *)
+let lns = (entry | empty | comment)* . table*

lenses/tests/test_toml.aug
@@ -1,0 +1,323 @@
+module Test_Toml =
+
+(* Test: Toml.norec
+     String value *)
+test Toml.norec get "\"foo\"" = { "string" = "foo" }
+
+(* Test: Toml.norec
+     Integer value *)
+test Toml.norec get "42" = { "integer" = "42" }
+
+(* Test: Toml.norec
+     Positive integer value *)
+test Toml.norec get "+42" = { "integer" = "+42" }
+
+(* Test: Toml.norec
+     Negative integer value *)
+test Toml.norec get "-42" = { "integer" = "-42" }
+
+(* Test: Toml.norec
+     Large integer value *)
+test Toml.norec get "5_349_221" = { "integer" = "5_349_221" }
+
+(* Test: Toml.norec
+     Hexadecimal integer value *)
+test Toml.norec get "0xDEADBEEF" = { "integer" = "0xDEADBEEF" }
+
+(* Test: Toml.norec
+     Octal integer value *)
+test Toml.norec get "0o755" = { "integer" = "0o755" }
+
+(* Test: Toml.norec
+     Binary integer value *)
+test Toml.norec get "0b11010110" = { "integer" = "0b11010110" }
+
+(* Test: Toml.norec
+     Float value *)
+test Toml.norec get "3.14" = { "float" = "3.14" }
+
+(* Test: Toml.norec
+     Positive float value *)
+test Toml.norec get "+3.14" = { "float" = "+3.14" }
+
+(* Test: Toml.norec
+     Negative float value *)
+test Toml.norec get "-3.14" = { "float" = "-3.14" }
+
+(* Test: Toml.norec
+     Complex float value *)
+test Toml.norec get "-3_220.145_223e-34" = { "float" = "-3_220.145_223e-34" }
+
+(* Test: Toml.norec
+     Inf float value *)
+test Toml.norec get "-inf" = { "float" = "-inf" }
+
+(* Test: Toml.norec
+     Nan float value *)
+test Toml.norec get "-nan" = { "float" = "-nan" }
+
+(* Test: Toml.norec
+     Bool value *)
+test Toml.norec get "true" = { "bool" = "true" }
+
+(* Test: Toml.norec
+     Datetime value *)
+test Toml.norec get "1979-05-27T07:32:00Z" =
+  { "datetime" = "1979-05-27T07:32:00Z" }
+test Toml.norec get "1979-05-27 07:32:00.999999" =
+  { "datetime" = "1979-05-27 07:32:00.999999" }
+
+(* Test: Toml.norec 
+     Date value *)
+test Toml.norec get "1979-05-27" =
+  { "date" = "1979-05-27" }
+
+(* Test: Toml.norec 
+     Time value *)
+test Toml.norec get "07:32:00" =
+  { "time" = "07:32:00" }
+
+(* Test: Toml.norec
+     String value with newline *)
+test Toml.norec get "\"bar\nbaz\"" =
+  { "string" = "bar\nbaz" }
+
+(* Test: Toml.norec
+     Multiline value *)
+test Toml.norec get "\"\"\"\nbar\nbaz\n    \"\"\"" =
+  { "string_multi" = "bar\nbaz" }
+
+(* Test: Toml.norec
+     Literal string value *)
+test Toml.norec get "'bar\nbaz'" =
+  { "string_literal" = "bar\nbaz" }
+
+(* Test: Toml.array_norec
+     Empty array *)
+test Toml.array_norec get "[ ]" =
+  { "array" {} }
+
+(* Test: Toml.array_norec
+     Array of strings *)
+test Toml.array_norec get "[ \"foo\", \"bar\" ]" =
+  { "array" {}
+    { "string" = "foo" } {}
+    { "string" = "bar" } {} }
+
+(* Test: Toml.array_norec
+     Array of arrays *)
+test Toml.array_rec get "[ [ \"foo\", [ 42, 43 ] ], \"bar\" ]" =
+  { "array" {}
+    { "array" {}
+      { "string" = "foo" } {}
+      { "array" {}
+        { "integer" = "42" } {}
+        { "integer" = "43" } {} } {} } {}
+    { "string" = "bar" } {} }
+
+(* Test: Toml.lns
+     Global parameters *)
+test Toml.lns get "# Globals
+foo = \"bar\"\n" =
+  { "#comment" = "Globals" }
+  { "entry" = "foo" { "string" = "bar" } }
+
+(* Test: Toml.lns
+     Simple section/value *)
+test Toml.lns get "[foo]
+bar = \"baz\"\n" =
+  { "table" = "foo" { "entry" = "bar" { "string" = "baz" } } }
+
+(* Test: Toml.lns
+     Subsections *)
+test Toml.lns get "[foo]
+title = \"bar\"
+  [foo.one]
+  hello = \"world\"\n" =
+  { "table" = "foo"
+    { "entry" = "title" { "string" = "bar" } } }
+  { "table" = "foo.one"
+    { "entry" = "hello" { "string" = "world" } } }
+
+(* Test: Toml.lns
+     Nested subsections *)
+test Toml.lns get "[foo]
+[foo.one]
+[foo.one.two]
+bar = \"baz\"\n" =
+  { "table" = "foo" }
+  { "table" = "foo.one" }
+  { "table" = "foo.one.two"
+    { "entry" = "bar" { "string" = "baz" } } }
+
+(* Test: Toml.lns
+     Arrays of tables *)
+test Toml.lns get "[[products]]
+name = \"Hammer\"
+sku = 738594937
+
+[[products]]
+
+[[products]]
+name = \"Nail\"
+sku = 284758393
+color = \"gray\"\n" =
+  { "@table" = "products"
+    { "entry" = "name"
+      { "string" = "Hammer" } }
+    { "entry" = "sku"
+      { "integer" = "738594937" } }
+    {  }
+  }
+  { "@table" = "products"
+    {  }
+  }
+  { "@table" = "products"
+    { "entry" = "name"
+      { "string" = "Nail" } }
+    { "entry" = "sku"
+      { "integer" = "284758393" } }
+    { "entry" = "color"
+      { "string" = "gray" } }
+  }
+
+(* Test: Toml.entry
+     Empty inline table *)
+test Toml.entry get "name = { }\n" =
+  { "entry" = "name"
+    { "inline_table"
+      {  } } }
+
+(* Test: Toml.entry
+     Inline table *)
+test Toml.entry get "name = { first = \"Tom\", last = \"Preston-Werner\" }\n" =
+  { "entry" = "name"
+    { "inline_table" {}
+      { "entry" = "first"
+        { "string" = "Tom"  } } {}
+      { "entry" = "last"
+        { "string" = "Preston-Werner"  } } {} } }
+
+(* Variable: example
+     The example from https://github.com/mojombo/toml *)
+let example = "# This is a TOML document. Boom.
+
+title = \"TOML Example\"
+
+[owner]
+name = \"Tom Preston-Werner\"
+organization = \"GitHub\"
+bio = \"GitHub Cofounder & CEO\nLikes tater tots and beer.\"
+dob = 1979-05-27T07:32:00Z # First class dates? Why not?
+
+[database]
+server = \"192.168.1.1\"
+ports = [ 8001, 8001, 8002 ]
+connection_max = 5000
+enabled = true
+
+[servers]
+
+  # You can indent as you please. Tabs or spaces. TOML don't care.
+  [servers.alpha]
+  ip = \"10.0.0.1\"
+  dc = \"eqdc10\"
+
+  [servers.beta]
+  ip = \"10.0.0.2\"
+  dc = \"eqdc10\"
+  country = \"中国\" # This should be parsed as UTF-8
+
+[clients]
+data = [ [\"gamma\", \"delta\"], [1, 2] ] # just an update to make sure parsers support it
+
+# Line breaks are OK when inside arrays
+hosts = [
+  \"alpha\",
+  \"omega\"
+]
+
+# Products
+
+  [[products]]
+  name = \"Hammer\"
+  sku = 738594937
+
+  [[products]]
+  name = \"Nail\"
+  sku = 284758393
+  color = \"gray\"
+"
+
+test Toml.lns get example =
+  { "#comment" = "This is a TOML document. Boom." } {  }
+  { "entry" = "title"
+    { "string" = "TOML Example" } } {  }
+  { "table" = "owner"
+    { "entry" = "name"
+      { "string" = "Tom Preston-Werner" } }
+    { "entry" = "organization"
+      { "string" = "GitHub" } }
+    { "entry" = "bio"
+      { "string" = "GitHub Cofounder & CEO
+Likes tater tots and beer." }
+    }
+    { "entry" = "dob"
+      { "datetime" = "1979-05-27T07:32:00Z" }
+      { "#comment" = "First class dates? Why not?" } } {  } }
+  { "table" = "database"
+    { "entry" = "server"
+      { "string" = "192.168.1.1" } }
+    { "entry" = "ports"
+      { "array" {  }
+        { "integer" = "8001" } {  }
+        { "integer" = "8001" } {  }
+        { "integer" = "8002" } {  } } }
+    { "entry" = "connection_max"
+      { "integer" = "5000" } }
+    { "entry" = "enabled"
+      { "bool" = "true" } } {  } }
+  { "table" = "servers" {  }
+    { "#comment" = "You can indent as you please. Tabs or spaces. TOML don't care." } }
+  { "table" = "servers.alpha"
+    { "entry" = "ip"
+      { "string" = "10.0.0.1" } }
+    { "entry" = "dc"
+      { "string" = "eqdc10" } } {  } }
+  { "table" = "servers.beta"
+    { "entry" = "ip"
+      { "string" = "10.0.0.2" } }
+    { "entry" = "dc"
+      { "string" = "eqdc10" } }
+    { "entry" = "country"
+      { "string" = "中国" }
+      { "#comment" = "This should be parsed as UTF-8" } } {  } }
+  { "table" = "clients"
+    { "entry" = "data"
+      { "array" {  }
+        { "array"
+          { "string" = "gamma" } {  }
+          { "string" = "delta" } } {  }
+        { "array"
+          { "integer" = "1" } {  }
+          { "integer" = "2" } } {  } }
+      { "#comment" = "just an update to make sure parsers support it" } } {  }
+    { "#comment" = "Line breaks are OK when inside arrays" }
+    { "entry" = "hosts"
+      { "array" {  }
+        { "string" = "alpha" } {  }
+        { "string" = "omega" } {  } } } {  }
+    { "#comment" = "Products" } {  } }
+  { "@table" = "products"
+    { "entry" = "name"
+      { "string" = "Hammer" } }
+    { "entry" = "sku"
+      { "integer" = "738594937" } } {  } }
+  { "@table" = "products"
+    { "entry" = "name"
+      { "string" = "Nail" } }
+    { "entry" = "sku"
+      { "integer" = "284758393" } }
+    { "entry" = "color"
+      { "string" = "gray" } } }
+

tests/Makefile.am
@@ -218,6 +218,7 @@ lens_tests =			\
   lens-thttpd.sh		\
   lens-tmpfiles.sh		\
   lens-trapperkeeper.sh		\
+  lens-toml.sh		\
   lens-tuned.sh			\
   lens-up2date.sh		\
   lens-updatedb.sh		\

commit a7e5296ceeb1a56a3af386747fd11cc190ed8414

fix(lens): toml: write support (#742)
* fix(lens): toml: add write support

The toml lens fails to save simple toml files due to problems with multi
dimensional (>2) arrays.
Therefore disable arrays with more than 2 dimensions for now to enable
basic usage again.

This commit fixes issue #715 by applying the suggested workaround. If
anyone can come up with a better solution please feel free to do so.

Fixes: 5e1cd313 (TOML lens (#91), 2018-11-29)
Signed-off-by: Richard Leitner <richard.leitner@skidata.com>

* fix(lens): toml: update reference link

toml.io now provides a versioned reference URL, therefore use this
instead of the old GitHub repo link.

Signed-off-by: Richard Leitner <richard.leitner@skidata.com>

* fix(lens): toml: typo in About: License

Signed-off-by: Richard Leitner <richard.leitner@skidata.com>

* test(lens): toml: add basic write

This test covers the bug described in issue #715.

Signed-off-by: Richard Leitner <richard.leitner@skidata.com>

* test(lens): toml: fix "array of arrays"

The "Test: Toml.array_norec; Array of arrays" tested a 3 dimensional
array. This commit fixes the test to actually test a 2 dimensional
array.

More than 2 dimensional arrays are currently not supported due to
issue #715.

Signed-off-by: Richard Leitner <richard.leitner@skidata.com>

Co-authored-by: George Hansper <george-github@hansper.id.au>

lenses/toml.aug
@@ -5,10 +5,10 @@ Module: Toml
 Author: Raphael Pinson <raphael.pinson@camptocamp.com>
 
 About: Reference
-  https://github.com/mojombo/toml/blob/master/README.md
+  https://toml.io/en/v1.0.0
 
 About: License
-   This file is licenced under the LGPL v2+, like the rest of Augeas.
+   This file is licensed under the LGPL v2+, like the rest of Augeas.
 
 About: Lens Usage
    To be documented
@@ -111,7 +111,9 @@ let array (value:lens) = [ label "array" . lbrack
 
 let array_norec = array norec
 
-let rec array_rec = array (norec | array_rec)
+(* This is actually no real recursive array, instead it is one or two dimensional
+   For more info on this see https://github.com/hercules-team/augeas/issues/715 *)
+let array_rec = array (norec | array_norec)
 
 let entry_base (value:lens) = [ label "entry" . store Rx.word . Sep.space_equal . value ]
 

lenses/tests/test_toml.aug
@@ -104,16 +104,14 @@ test Toml.array_norec get "[ \"foo\", \"bar\" ]" =
     { "string" = "foo" } {}
     { "string" = "bar" } {} }
 
-(* Test: Toml.array_norec
+(* Test: Toml.array_rec
      Array of arrays *)
-test Toml.array_rec get "[ [ \"foo\", [ 42, 43 ] ], \"bar\" ]" =
+test Toml.array_rec get "[ [ \"foo\", \"bar\" ], 42 ]" =
   { "array" {}
     { "array" {}
       { "string" = "foo" } {}
-      { "array" {}
-        { "integer" = "42" } {}
-        { "integer" = "43" } {} } {} } {}
-    { "string" = "bar" } {} }
+      { "string" = "bar" } {} } {}
+    { "integer" = "42" } {} }
 
 (* Test: Toml.lns
      Global parameters *)
@@ -331,3 +329,14 @@ Likes tater tots and beer." }
     { "entry" = "color"
       { "string" = "gray" } } }
 
+(* Variable: minimal_toml *)
+let minimal_toml = "[root]
+foo = \"bar\"
+"
+
+(* Test: minimal write *)
+test Toml.lns put minimal_toml after
+    set "/table[1]/entry[1]/string" "foo"
+  = "[root]
+foo = \"foo\"
+"