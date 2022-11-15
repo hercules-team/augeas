c7aa09b66decee2627dc30bf73f338e13de14609
George Hansper <george@hansper.id.au>
2022-11-15 21:24:10 +1100

    Allow unmatched square-brackets in values

---
+++ tests/test-augtool/quoted-strings.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#insert after /files/tmp/test-quoted-strings.txt/*[. =~ regexp('find.*')] 'found ['
+commands="
+set /files/var/tmp/test-quoted-strings.txt/01 '['
+set /files/var/tmp/test-quoted-strings.txt/02 ']'
+set /files/var/tmp/test-quoted-strings.txt/03 ']['
+set /files/var/tmp/test-quoted-strings.txt/04 '[]'
+set /files/var/tmp/test-quoted-strings.txt/05 '[]['
+set /files/var/tmp/test-quoted-strings.txt/06 '[]]'
+set /files/var/tmp/test-quoted-strings.txt/07 '('
+set /files/var/tmp/test-quoted-strings.txt/08 ')'
+set /files/var/tmp/test-quoted-strings.txt/09 '"\""'
+insert 010 after /files/var/tmp/test-quoted-strings.txt/seq::*[.=~regexp('.*\]\]\[ \)\( .*')]
+set /files/var/tmp/test-quoted-strings.txt/010 'found ]][ )('
+insert 011 after /files/var/tmp/test-quoted-strings.txt/seq::*[.=~regexp('.*\\\..*')]
+set /files/var/tmp/test-quoted-strings.txt/011 'found .'
+"
+lens=Simplelines.lns
+file="/var/tmp/test-quoted-strings.txt"
+
+diff='--- /var/tmp/test-quoted-strings.txt
++++ /var/tmp/test-quoted-strings.txt.augnew
+@@ -1 +1,12 @@
+ find this: ]][ )( . * $ / \
++found .
++found ]][ )(
++[
++]
++][
++[]
++[][
++[]]
++(
++)
++"'
