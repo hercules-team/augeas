6c106d96f921a1747daa9f9a4358bfdcb626bcaf
George Hansper <george@hansper.id.au>
2022-12-07 21:12:08 +1100

    Version 1.14.0 - addenda: add augprint to augeas.spec.in, fix distcheck

--- tests/test-augprint.sh
+++ tests/test-augprint.sh
@@ -3,29 +3,39 @@
 # Test that augprint produces the expected output, and that the output
 # can be used to reconstruct the original file
 
-root=$abs_top_builddir/build/test-augprint
-augeas_root=$abs_top_builddir/tests/test-augprint
 
-export AUGEAS_LENS_LIB=$abs_top_builddir/lenses
+test_augprint_files=$abs_top_srcdir/tests/test-augprint
+
+export AUGEAS_ROOT=$abs_top_builddir/build/test-augprint
+export AUGEAS_LENS_LIB=$abs_top_srcdir/lenses
+
 # ------------- /etc/hosts ------------
-mkdir -p $root/etc
-AUGEAS_ROOT=$augeas_root augprint --pretty --verbose /etc/hosts > $root/etc.hosts.augtool
-AUGEAS_ROOT=$root augtool --load-file=/etc/hosts -f  $root/etc.hosts.augtool --noload --autosave 1>/dev/null
-diff -bu    $augeas_root/etc.hosts.pretty.augtool $root/etc.hosts.augtool || exit 1
-diff -bu -B $augeas_root/etc/hosts $root/etc/hosts || exit 1
+mkdir -p $AUGEAS_ROOT/etc
+cp --no-preserve=mode --recursive $test_augprint_files/etc/ $AUGEAS_ROOT/
 
-AUGEAS_ROOT=$augeas_root augprint --regexp=2 /etc/hosts > $root/etc.hosts.augtool
-AUGEAS_ROOT=$root augtool --load-file=/etc/hosts -f  $root/etc.hosts.augtool --noload --autosave 1>/dev/null
-diff -bu    $augeas_root/etc.hosts.regexp.augtool $root/etc.hosts.augtool || exit 1
-diff -bu -B $augeas_root/etc/hosts $root/etc/hosts || exit 1
+output=$AUGEAS_ROOT/etc.hosts.augprint
+AUGEAS_ROOT=$test_augprint_files augprint --pretty --verbose /etc/hosts > $output
+augtool --load-file=/etc/hosts -f  $output --autosave 1>/dev/null
+# Check that output matches expected output
+diff -bu    $test_augprint_files/etc.hosts.pretty.augprint $output || exit 1
+# Check that file is unchanged
+diff -bu -B $test_augprint_files/etc/hosts $AUGEAS_ROOT/etc/hosts || exit 1
+
+AUGEAS_ROOT=$test_augprint_files augprint --regexp=2 /etc/hosts > $output
+augtool --load-file=/etc/hosts -f  $output --autosave 1>/dev/null
+# Check that output matches expected output
+diff -bu    $test_augprint_files/etc.hosts.regexp.augprint $output || exit 1
+# Check that file is unchanged
+diff -bu -B $test_augprint_files/etc/hosts $AUGEAS_ROOT/etc/hosts || exit 1
 
 # ------------- /etc/squid/squid.conf and /etc/pam,d/systemd-auth  ------------
 for test_file in /etc/squid/squid.conf /etc/pam.d/system-auth ; do
-	mkdir -p $(dirname $root/$test_file)
+	mkdir -p $(dirname $AUGEAS_ROOT/$test_file)
   output=${test_file//\//.}
-  output=${output#.}
-	AUGEAS_ROOT=$augeas_root augprint $test_file > $root/${output}.augtool
-	AUGEAS_ROOT=$root augtool --load-file=$test_file -f  $root/${output}.augtool --noload --autosave 1>/dev/null
-	diff -bu -B $augeas_root/$test_file $root/$test_file || exit 1
+  output=${AUGEAS_ROOT}/${output#.}.augprint
+	AUGEAS_ROOT=$test_augprint_files augprint $test_file > $output
+	augtool --load-file=$test_file -f $output --autosave 1>/dev/null
+	# Check that file is unchanged
+	diff -bu -B $test_augprint_files/$test_file $AUGEAS_ROOT/$test_file || exit 1
 done
 
